f0fc44c90f922ec1cace6c4334735c5ff35b585e
Instructor <vokunev@amazon.com>
2022-12-11 01:54:33 -0800

    Added comments

--- s3/presign.sh
+++ s3/presign.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+#
+# This script creates Amazon S3 pre-signed URL for the object soma_smoothie.jpg stored in product-catalog-images bucket. The URL expires in 15 seconds.
+#
+# To run this script:
+# chmod u+x presign.sh
+# ./presign.sh
+#
 aws s3 presign s3://product-catalog-images/soma_smoothie.jpg --expires-in 15
