3436359b7b76b3c570c536474f22fbfa8f8f8c09
Instructor <vokunev@amazon.com>
2023-02-07 14:46:57 -0800

    Updated comments

--- cloudfront/redirect_apex_to_www.js
+++ cloudfront/redirect_apex_to_www.js
@@ -1,5 +1,5 @@
 /*
-* This Amazon CloudFront function performs 301 Moved Permanently redirect of GET requests made to the
+* This Amazon CloudFront function performs "301 Moved Permanently" redirect of GET requests made to the
 * apex (root) domain to www subdomain. It preserves the original path and the query string.
 *
 * For simplicity sake, the other types of requests are not handled here. E.g. for POST methods, 
